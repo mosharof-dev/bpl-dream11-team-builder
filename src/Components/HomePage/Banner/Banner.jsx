9c1fc28f4d255bb3ee096706827238fc0a390cba
programmermosharof <mosharof.dev@gmail.com>
2026-03-27 23:28:28 +0600

    Banner Section somplete

--- src/Components/HomePage/Banner/Banner.jsx
+++ src/Components/HomePage/Banner/Banner.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import BannerLog from '../../../assets/banner-main.png'
+
+import BannerBg from '../../../assets/bg-shadow.png'
 
 const Banner = () => {
     return (
-        <div className=' bg-green-700'>
-             <div className=' h-80 container mx-auto'>
-            <h1 className='text-center font-bold text-7xl'>Hero Section Coming Soon </h1>
-            
-        </div>
-        </div>
+
+<div className='container mx-auto mt-4 rounded-2xl bg-cover bg-center bg-no-repeat bg-black'
+            style={{ backgroundImage: `url(${BannerBg})` }}>
+<div className='py-18 flex flex-col text-center items-center justify-center space-y-4  text-white'>
+    <img className='' src={BannerLog} alt={"Banner Logo"} />
+
+    <h1 className='font-bold text-4xl'>Assemble Your Ultimate Dream 11 Cricket Team</h1>
+    <p className='font-bold text-2xl'>Beyond Boundaries Beyond Limits</p>
+    <button className='banner-btn font-bold'>Claim Free Credit</button>
+
+    
+
+</div>
+</div>
+
        
     );
 };
